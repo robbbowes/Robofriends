85ce059e178e6dfcd23ca6cf5a957649b0d272bd
Robb Bowes <up666724@myport.ac.uk>
2019-01-12 19:19:39 +0100

    Add CardList component, map robots objects

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { robots } from './robots';
 
 ReactDOM.render(
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} robots/>, 
-    document.getElementById('root'));
+    <CardList robots={robots}/>, document.getElementById('root'));
 serviceWorker.unregister();
