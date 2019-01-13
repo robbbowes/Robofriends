c9c24cea5c744972a2d6e25945988550dde7a451
Robb Bowes <up666724@myport.ac.uk>
2019-01-13 01:12:40 +0100

    Remove robots.js fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
@@ -9,14 +8,24 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: ''
     }
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => {
+      return response.json();
+    })
+    .then(users => {
+      this.setState({ robots: users })
+    })
+  };
+
   onSearchChange = (event) => {
     this.setState({ searchField: event.target.value });
-;  }
+  }
  
   render() {
     const filteredRobots = this.state.robots.filter(robots => {
